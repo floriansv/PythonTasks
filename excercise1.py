8fe4cc028eec18f2bfaf4eada38f663a0657cefd
floriags <floriags@student.matnat.uio.no>
2018-08-23 14:08:21 +0200

    Veldig nyttig melding

---
+++ excercise1.py
@@ -0,0 +1,8 @@
+"""
+Write a function less_than(original, n) that takes a list of integers (original) anda number  n,
+and returns a new list of the elements in the original list that were smaller than  n.
+Your function should use a list comprehension to do this.
+Write a few simple tests of your function.
+"""
+
+print("Hello world!")
